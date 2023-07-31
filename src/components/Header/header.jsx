640618dd960d07e78477d83e2517653c647b79d7
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-07-31 14:16:03 +0200

    maj style

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
+/*import styled from 'styled-components';*/
 import Logo from "../../assets/logo";
 
-const StyledLink = styled(Link)`
+/*const StyledLink = styled(Link)`
     padding: 15px;
     color: #ff6060;
     text-decoration: none;
     font-size: 18px;
     font-family: 'Montserrat', sans-serif;
-`
+`*/
 function Header() {
     return(
-    <div class="header">
+    <div className="header">
         <Logo />
         <nav>
-            <StyledLink to="/">Accueil</StyledLink>
-            <StyledLink to="/about">A propos</StyledLink>
+            <Link className="nav-link" to="/">Accueil</Link>
+            <Link className="nav-link" to="/about">A propos</Link>
         </nav>
     </div>
     )
