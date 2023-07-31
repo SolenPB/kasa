27024cd53a9afc6a17e2f72ef2a5ab01f52c85ef
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-07-31 14:32:50 +0200

    maj style gallery

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,10 @@
 import img1 from '../../assets/img1.png'
 
 function Home() {
-    return (
+    return (<div className='title-gallery'>
     <img className="sea"src={img1} alt="Vue sur la mer"/>
+    <p className='title-sea'>Chez vous, partout et ailleurs</p>
+    </div>
     )
   }
 
