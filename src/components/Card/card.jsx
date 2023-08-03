8828969368ac39fd3e44322c433f81b6f5a33ab8
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-03 18:00:32 +0200

    maj style

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,8 +1,8 @@
 function Card(props) {
     return(
-   <div className="gallery-cards" href="#">
-            <img src={props.picture} alt="logement" className="picture" />
-            <span className="title-card">{props.title}</span>
+   <div className="gallery__gallery-cards" href="#">
+            <img src={props.picture} alt="logement" className="gallery-cards__picture" />
+            <span className="gallery-cards__title-card">{props.title}</span>
     </div>
     )
 }
