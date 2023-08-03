d3c92a9675a4293cda60d53dcca77ac07dc57ed0
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-03 20:26:06 +0200

    maj style cards

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -3,6 +3,7 @@ function Card(props) {
    <div className="gallery__gallery-cards" href="#">
             <img src={props.picture} alt="logement" className="gallery__gallery-cards__picture" />
             <span className="gallery__gallery-cards__title-card">{props.title}</span>
+            <div className="gallery__gallery-cards__overlay"></div>
     </div>
     )
 }
