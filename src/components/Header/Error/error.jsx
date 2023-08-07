083f83a0b9d53aeaeda92981991084e700cc2d10
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-07 22:49:32 +0200

    maj style error

--- src/components/Header/Error/error.jsx
+++ src/components/Header/Error/error.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
+
 function Error() {
     return(
         <div className="error">
             <h1 className="error__error-title">404</h1>
-            <h2 className="error-message">Oups ! La page que vous demandez n'existe pas.</h2>
+            <h2 className="error__error-message">Oups ! La page que vous demandez n'existe pas.</h2>
+            <Link className="error__error-return" to='/' >Retourner à la page d'accueil</Link>
         </div>
     )
 }
