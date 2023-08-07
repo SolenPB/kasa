5d0fbc60e3d17260028c3f9b5a336a8dc9b60a1f
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-07 13:10:20 +0200

    maj carrousel

--- src/pages/Carrousel/carrousel.jsx
+++ src/pages/Carrousel/carrousel.jsx
@@ -1,11 +1,17 @@
+import Card from '../../components/Card/card';
+
 function Carrousel() {
     let data = require('../../datas/destination.json');
-    data = data.slice(0,6);
+    data = data.slice(0,1);
     console.log(data);
-    
+
     return(
-        <div>
-    
+        <div className='description'>
+            {data.map((data, index) => {
+             return <div className="">
+                key={index} title={data.title} picture={data.cover}
+             </div> 
+           })}
         </div>
     )
 }
