05c4988f7cad9fcee8d526a30e6dea558453e53d
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-07 10:34:58 +0200

    maj style list

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -2,12 +2,12 @@ function Collapse(){
 
     return(
         <div className="list">
-            <div className="list-menu">
+            <div className="list__list-menu">
                 <div className="list-button ">
                     <h2 className="list-button__list-title">Fiabilité</h2>
                     <button></button>
                 </div>
-                <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+                <p className="list__list-description">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
                 régulièrement vérifiées par nos équipes.</p>
             </div>
 
@@ -16,7 +16,7 @@ function Collapse(){
                     <h2 className="list-button__list-title">Respect</h2>
                     <button></button>
                 </div>
-                <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+                <p className="list__list-description">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
                 perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
             </div>
 
@@ -25,7 +25,7 @@ function Collapse(){
                     <h2 className="list-button__list-title">Service</h2>
                     <button></button>
                 </div>
-                <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+                <p className="list__list-description">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
                 perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
             </div>
 
@@ -34,7 +34,7 @@ function Collapse(){
                     <h2 className="list-button__list-title">Sécurité</h2>
                     <button></button>
                 </div>
-                <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+                <p className="list__list-description">La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
                 correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
                 locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
                 également des ateliers sur la sécurité domestique pour nos hôtes.</p>
