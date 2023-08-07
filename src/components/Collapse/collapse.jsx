98d4eb6e06c8e61c168b2ddb20565850e53682bb
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-07 09:59:33 +0200

    maj style & collapse

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -2,27 +2,37 @@ function Collapse(){
 
     return(
         <div className="list">
-            <h2 className="list-title">Fiabilité</h2>
-            <button></button>
-            <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
-            régulièrement vérifiées par nos équipes.</p>
+            <div className="list-menu">
+                <div className="list-button">
+                    <h2 className="list-title">Fiabilité</h2>
+                    <button></button>
+                </div>
+                <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+                régulièrement vérifiées par nos équipes.</p>
+            </div>
 
-            <h2 className="list-title">Respect</h2>
-            <button></button>
-            <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-            perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+            <div className="list-menu">
+                <h2 className="list-title">Respect</h2>
+                <button></button>
+                <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+                perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+            </div>
 
-            <h2 className="list-title">Service</h2>
-            <button></button>
-            <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-            perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+            <div className="list-menu">
+                <h2 className="list-title">Service</h2>
+                <button></button>
+                <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+                perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+            </div>
 
-            <h2 className="list-title">Sécurité</h2>
-            <button></button>
-            <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
-            correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
-            locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
-            également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+            <div className="list-menu">
+                <h2 className="list-title">Sécurité</h2>
+                <button></button>
+                <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+                correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+                locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
+                également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+            </div>
         </div>
     )
 }
