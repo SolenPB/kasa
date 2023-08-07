8502f3fe8fd2933e5ae5ade066ab1bbc29c4f51a
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-07 11:10:05 +0200

    maj style collapse

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -5,7 +5,7 @@ function Collapse(){
             <div className="list__list-menu">
                 <div className="list-button ">
                     <h2 className="list-button__list-title">Fiabilité</h2>
-                    <button>︿</button>
+                    <button className="list-button__action-button">︿</button>
                 </div>
                 <p className="list__list-description">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
                 régulièrement vérifiées par nos équipes.</p>
@@ -14,7 +14,7 @@ function Collapse(){
             <div className="list-menu">
                 <div className="list-button">
                     <h2 className="list-button__list-title">Respect</h2>
-                    <button>︿</button>
+                    <button className="list-button__action-button">︿</button>
                 </div>
                 <p className="list__list-description">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
                 perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
@@ -23,7 +23,7 @@ function Collapse(){
             <div className="list-menu">
                 <div className="list-button">
                     <h2 className="list-button__list-title">Service</h2>
-                    <button>︿</button>
+                    <button className="list-button__action-button">︿</button>
                 </div>
                 <p className="list__list-description">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
                 perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
@@ -32,7 +32,7 @@ function Collapse(){
             <div className="list-menu">
                 <div className="list-button">
                     <h2 className="list-button__list-title">Sécurité</h2>
-                    <button>︿</button>
+                    <button className="list-button__action-button">︿</button>
                 </div>
                 <p className="list__list-description">La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
                 correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
