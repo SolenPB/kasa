4593f9bb437e7ac81fb75683a2e79d16864f5815
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-08 18:36:14 +0200

    maj about

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -8,55 +8,135 @@ function Collapse(){
     const[isOpenService, setIsOpenService] = useState(false);
     const[isOpenSecurity, setIsOpenSecurity] = useState(false);
 
-    return(
-        <div className="list">
-            <div className="list-menu">
-                <div className="list-button ">
-                    <h2 className="list-button__list-title">Fiabilité</h2>
-                    <button className="list-button__action-button" onClick={() => setIsOpenReliability(true)}><FontAwesomeIcon icon={faChevronUp} /></button>
-                </div>
-                {isOpenReliability ? (                
-                    <p className="list__list-description">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
-                régulièrement vérifiées par nos équipes.</p>)
-                : null}
+//Fonctions pour les ouvertures 
 
+    function OpenReliability(){
+        setIsOpenReliability(true);
+    }
+    function OpenRespect(){
+        setIsOpenRespect(true);
+    }
+    function OpenService(){
+        setIsOpenService(true);
+    }
+    function OpenSecurity(){
+        setIsOpenSecurity(true);
+    }
+
+//Fonctions pour les fermetures 
+
+    function ClosedReliability(){
+        setIsOpenReliability(false);
+    }
+    function ClosedRespect(){
+        setIsOpenRespect(false);
+    }
+    function ClosedService(){
+        setIsOpenService(false);
+    }
+    function ClosedSecurity(){
+        setIsOpenSecurity(false)
+    }
+
+    useEffect(() => {
+        OpenReliability();
+        ClosedReliability();
+        OpenRespect();
+        ClosedRespect();
+        OpenService();
+        ClosedService();
+        OpenSecurity();
+        ClosedSecurity();
+    }, [])
+
+    return(
+
+    //Condition d'ouverture et de fermeture du bouton "Fiabilité"
+
+        <div className="list">
+        {isOpenReliability ? (
+            <div className="menu">
+                <div className='list-button'>
+                    <h2 className="list-button__list-title">Fiabilité</h2>
+                    <button className="list-button__action-button" onClick={() => ClosedReliability()}><FontAwesomeIcon icon={faChevronUp} /></button>
+                </div>
+                <p className="list__list-description">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+                régulièrement vérifiées par nos équipes.</p>
             </div>
 
-            <div className="list-menu">
+        ) : (
+            <div className="menu">
+                <div className='list-button'>
+                    <h2 className="list-button__list-title">Fiabilité</h2>
+                    <button className="list-button__action-button" onClick={() => OpenReliability()}><FontAwesomeIcon icon={faChevronUp} /></button>
+                </div>
+            </div>
+        )}
+        
+    {/*Condition d'ouverture et de fermeture du bouton "Respect */}
+
+        {isOpenRespect ? (
+            <div className="menu">
                 <div className="list-button">
                     <h2 className="list-button__list-title">Respect</h2>
-                    <button className="list-button__action-button" onClick={() => setIsOpenRespect(true)}><FontAwesomeIcon icon={faChevronUp} /></button>
-                </div>
-                {isOpenRespect ? (
+                    <button className="list-button__action-button" onClick={() => ClosedRespect()}><FontAwesomeIcon icon={faChevronUp} /></button>
+                </div>   
                 <p className="list__list-description">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-                perturbation du voisinage entraînera une exclusion de notre plateforme.</p>)
-                : null}
+                perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
             </div>
 
+        ) : (
+            <div className="menu">
+                <div className='list-button'>
+                    <h2 className="list-button__list-title">Respect</h2>
+                    <button className="list-button__action-button" onClick={() => OpenRespect()}><FontAwesomeIcon icon={faChevronUp} /></button>
+                </div>
+            </div>
+        )}
+
+    {/*Condition d'ouverture et de fermeture du bouton "Service"*/}
+
+        {isOpenService ? (
             <div className="list-menu">
                 <div className="list-button">
                     <h2 className="list-button__list-title">Service</h2>
-                    <button className="list-button__action-button" onClick={() => setIsOpenService(true)}><FontAwesomeIcon icon={faChevronUp} /></button>
+                    <button className="list-button__action-button" onClick={() => ClosedService()}><FontAwesomeIcon icon={faChevronUp} /></button>
                 </div>
-                {isOpenService ? (
                 <p className="list__list-description">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-                perturbation du voisinage entraînera une exclusion de notre plateforme.</p>)
-                : null}
+                perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
             </div>
+        ) : (
+            <div className="menu">
+                <div className='list-button'>
+                    <h2 className="list-button__list-title">Service</h2>
+                    <button className="list-button__action-button" onClick={() => OpenService()}><FontAwesomeIcon icon={faChevronUp} /></button>
+                </div>
+            </div>
+        )}
 
+    {/*Condition d'ouverture et de fermeture du bouton "Sécurité" */}
+
+        {isOpenSecurity ? (
             <div className="list-menu">
                 <div className="list-button">
                     <h2 className="list-button__list-title">Sécurité</h2>
-                    <button className="list-button__action-button" onClick={() => setIsOpenSecurity(true)}><FontAwesomeIcon icon={faChevronUp} /></button>
+                    <button className="list-button__action-button" onClick={() => ClosedSecurity()}><FontAwesomeIcon icon={faChevronUp} /></button>
                 </div>
-                {isOpenSecurity ? (
                 <p className="list__list-description">La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
                 correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
                 locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
-                également des ateliers sur la sécurité domestique pour nos hôtes.</p>)
-                : null}
+                également des ateliers sur la sécurité domestique pour nos hôtes.</p>
             </div>
+        ) : (
+            <div className="menu">
+                <div className='list-button'>
+                    <h2 className="list-button__list-title">Sécurité</h2>
+                    <button className="list-button__action-button" onClick={() => OpenSecurity()}><FontAwesomeIcon icon={faChevronUp} /></button>
+                </div>
+            </div>
+        )}
         </div>
+
     )
 }
 
