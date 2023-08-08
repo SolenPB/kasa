bedbb6df387efcbbf1aaf7c367899a930c25cac4
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-08 15:15:17 +0200

    maj style about

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -3,14 +3,14 @@ import Collapse from '../../components/Collapse/collapse';
 
 function About() {
     return(
-        <>
+        <div className='about'>
             <div className="about-title">
                 <img src={img2} alt='Vue sur une chaîne de montagne et sa vallée' className='about-title__about-picture'/>
             </div>
             <div className='about-list'>
                 <Collapse />
             </div>
-        </>         
+        </div>         
 )
 }
 export default About;
