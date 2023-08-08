0a089ae49bba496727eadc1298f7e4e9f489d584
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-08 22:15:04 +0200

    maj style slide

--- src/components/Slideshow/slideshow.jsx
+++ src/components/Slideshow/slideshow.jsx
@@ -1,11 +1,14 @@
 function Slideshow(props) {
     return(
         <div className="slide">
-            <img src={props.picture} alt="logement"/>
-            <span>{props.title}</span>
-            <span>{props.host.name}</span>
-            <span>{props.location}</span>
-            <span>{props.tags}</span>
+            <img src={props.picture} alt="logement" className="slide__slide-pictures"/>
+            <span className="slide__slide-title">{props.title}</span>
+            <span className="slide__slide-location">{props.location}</span>
+
+            <div className="slide__host"><span>{props.host.name}</span>
+            <img src={props.host.picture} alt="Profil de l'hôte" className="slide__slide-host__host-picture" /></div>
+
+            <span className="slide__slide-tags">{props.tags}</span>
             <span>{props.rating}</span>
             <p>{props.description}</p>
             <span>{props.equipments}</span>
