04b0b3861f2b7c73ca05f9eba36aa485c07b4f40
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-10 10:48:18 +0200

    maj carousel

--- src/components/Slideshow/slideshow.jsx
+++ src/components/Slideshow/slideshow.jsx
@@ -1,12 +1,35 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Slideshow() {
     const data = require('../../datas/destination.json');
     const filter = useParams();
-    let dataFiltered ="";  
+    let dataFiltered ="";
     
+    const[isOpenDescription, setIsOpenDescription] = useState(false);
+    const[isOpenEquipments, setIsOpenEquipments] = useState(false);
+    function OpenDescription(){
+      setIsOpenDescription(true);
+    }
+    function CloseDescription(){
+      setIsOpenDescription(false);
+    }
+    function OpenEquipments(){
+      setIsOpenEquipments(true);
+    }
+    function CloseEquipments(){
+      setIsOpenEquipments(false);
+    }
+
+    useEffect(() => {
+      OpenDescription();
+      CloseDescription();
+      OpenEquipments();
+      CloseEquipments();
+    }, [])
+
     for( let i = 0; i < data.length; i++ ){
       if(filter.id === data[i].id) {
         dataFiltered = data[i];
@@ -15,7 +38,6 @@ function Slideshow() {
     return(
     <div className="slide">
         <Carousel showThumbs={false}>
-          
             {dataFiltered.pictures.map((data, index) => {
               return (<>
               <div className="slide-pictures">
@@ -35,13 +57,43 @@ function Slideshow() {
           </div>
           <div className="slide__rating">
             <span>{dataFiltered.rating}</span>
-          </div> 
-            <p>{dataFiltered.description}</p>
-            <span>{dataFiltered.equipments}</span>        
+          </div>
+          <div className="description">
+            {isOpenDescription ? (
+              <div className="description-title">
+                <h2>Description</h2>
+                <button className="btn-description" onClick={() => CloseDescription()}>^</button>
+                <p>{dataFiltered.description}</p>
+              </div>   
+            ) : (
+              <div className="description-title">
+                <h2>Description</h2>
+                <button className="btn-description" onClick={() => OpenDescription()}>^</button>
+              </div>
+            )
+              }
+          </div>   
+          <div className="equipments">
+          {isOpenEquipments ? (
+          <div className="equipment-title">
+            <h2 className="equipments-title">Équipements</h2>
+            <button className="btn-equipments" onClick={() => CloseEquipments()}>^</button>
+            <span>{dataFiltered.equipments}</span> 
+          </div>
+          ) : (
+            <div className="equipment-title">
+              <h2>Équipements</h2>
+              <button className="btn-equipments" onClick={() => OpenEquipments()}>^</button>
+            </div>
+          )
+
+          }
+            
+          </div>
     </div>
 
     )
     
-}
+}  
 
 export default Slideshow
