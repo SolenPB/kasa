ce41c3a8a0c8ba427750d33865e7af9b885b24ef
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-20 21:37:54 +0200

    maj gallery

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,8 +2,8 @@ import img1 from '../../assets/img1.png'
 
 function Home() {
     return (<div className='title-gallery'>      
-        <img className="sea"src={img1} alt="Vue sur la mer"/>     
-        <p className='title-sea'>
+        <img className="title-gallery__sea"src={img1} alt="Vue sur la mer"/>     
+        <p className='title-gallery__title-sea'>
           Chez vous, partout et ailleurs
         </p>
     </div>
