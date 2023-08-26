4b14d2ab21445dfd46129984efa7f69cbc4f23e7
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-26 14:14:15 +0200

    maj responsive error

--- src/components/Header/Error/error.jsx
+++ src/components/Header/Error/error.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 function Error() {
     return(
         <div className="error">
-            <h1 className="error__errorTitle">404</h1>
-            <h2 className="error__errorMessage">Oups ! La page que vous demandez n'existe pas.</h2>
-            <Link className="error__errorReturn" to='/' >Retourner à la page d'accueil</Link>
+            <h1 className="error__title">404</h1>
+            <h2 className="error__message">Oups ! La page que vous demandez n'existe pas.</h2>
+            <Link className="error__return" to='/' >Retourner à la page d'accueil</Link>
         </div>
     )
 }
