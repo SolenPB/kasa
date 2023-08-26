f4c8a5b38da5a99d4fa3358145e6483d0c5635e2
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-26 11:13:38 +0200

    maj responsive error

--- src/components/Header/Error/error.jsx
+++ src/components/Header/Error/error.jsx
@@ -5,7 +5,7 @@ function Error() {
         <div className="error">
             <h1 className="error__errorTitle">404</h1>
             <h2 className="error__errorMessage">Oups ! La page que vous demandez n'existe pas.</h2>
-            <Link className="error__errorRSeturn" to='/' >Retourner à la page d'accueil</Link>
+            <Link className="error__errorReturn" to='/' >Retourner à la page d'accueil</Link>
         </div>
     )
 }
