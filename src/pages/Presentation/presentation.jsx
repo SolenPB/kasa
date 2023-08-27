bf017fea0957abcb2e7675da0ce17d00a597e7f7
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-27 18:04:34 +0200

    maj presentation error

--- src/pages/Presentation/presentation.jsx
+++ src/pages/Presentation/presentation.jsx
@@ -18,6 +18,7 @@ export default function Slider() {
       equipments: [],
       tags: [],
     };
+    
     for( let i = 0; i < data.length; i++ ){
       if(filter.id === data[i].id) {
         dataFiltered = data[i];
