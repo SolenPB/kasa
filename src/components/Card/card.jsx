d09a74b385b5c6e730adb477ac6d0767e92a763c
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-08-27 11:05:21 +0200

    maj gallery

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -2,7 +2,6 @@ function Card(props) {
     return(
    <div className="gallery__galleryCards">
             <img src={props.picture} alt="Illustration de la location" className="gallery__galleryCards__picture" />
-                <div className="gallery__galleryCards__overlay"></div>
             <span className="gallery__galleryCards__titleCard">{props.title}</span>              
     </div>
     )
